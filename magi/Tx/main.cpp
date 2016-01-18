f29123640c6c630123911bc9d77bf685a12d4298
Omar Medjaouri <omedjaouri@gmail.com>
2016-01-18 16:20:02 -0600

    Added framework for testing receiver and transmitter

--- magi/Tx/main.cpp
+++ magi/Tx/main.cpp
@@ -1,101 +1,52 @@
-/*Copyright (c) 2009 Andrew Rapp. All rights reserved.
- *
- * This file is part of XBee-Arduino.
- *
- * XBee-Arduino is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * XBee-Arduino is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with XBee-Arduino.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include <Arduino.h>
 #include <XBee.h>
 
-/*
-This example is for Series 2 XBee
- Sends a ZB TX request with the value of analogRead(pin5) and checks the status response for success
-*/
-
-// create the XBee object
 XBee xbee = XBee();
 
-uint8_t payload[] = { 0, 0 };
+uint8_t payload[] = {0, 0};
 
-// SH + SL Address of receiving XBee
+//SH and SL Address of receiving Zigbee (Find using XCTU)
 XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x403e0f30);
 ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
 ZBTxStatusResponse txStatus = ZBTxStatusResponse();
 
-int pin5 = 0;
+int pinLED = 51;
+int pinComms = 0;
 
-int statusLed = 13;
-int errorLed = 13;
 
-void flashLed(int pin, int times, int wait) {
+void setup(){
+    pinMode(pinLED, OUTPUT);
 
-  for (int i = 0; i < times; i++) {
-    digitalWrite(pin, HIGH);
-    delay(wait);
-    digitalWrite(pin, LOW);
+    Serial.begin(9600);
+    xbee.setSerial(Serial);
 
-    if (i + 1 < times) {
-      delay(wait);
-    }
-  }
 }
 
-void setup() {
-  pinMode(statusLed, OUTPUT);
-  pinMode(errorLed, OUTPUT);
+void loop(){
+    pinComms = analogRead(5);
+    payload[0] = pinComms >> 8 & 0xff;
+    payload[1] = pinComms & 0xff;
 
-  Serial.begin(9600);
-  xbee.setSerial(Serial);
-}
+    xbee.send(zbTx);
 
-void loop() {   
-  // break down 10-bit reading into two bytes and place in payload
-  pin5 = analogRead(5);
-  payload[0] = pin5 >> 8 & 0xff;
-  payload[1] = pin5 & 0xff;
-
-  xbee.send(zbTx);
-
-  // flash TX indicator
-  flashLed(statusLed, 1, 100);
-
-  // after sending a tx request, we expect a status response
-  // wait up to half second for the status response
-  if (xbee.readPacket(500)) {
-    // got a response!
-
-    // should be a znet tx status               
-    if (xbee.getResponse().getApiId() == ZB_TX_STATUS_RESPONSE) {
-      xbee.getResponse().getZBTxStatusResponse(txStatus);
-
-      // get the delivery status, the fifth byte
-      if (txStatus.getDeliveryStatus() == SUCCESS) {
-        // success.  time to celebrate
-        flashLed(statusLed, 5, 50);
-      } else {
-        // the remote XBee did not receive our packet. is it powered on?
-        flashLed(errorLed, 3, 500);
-      }
+    digitalWrite(pinLED, HIGH);
+    
+    //Should get a status response after a TX request
+    //wait up to half a second for response
+    if (xbee.readPacket(500)){
+    
     }
-  } else if (xbee.getResponse().isError()) {
-    //nss.print("Error reading packet.  Error code: ");  
-    //nss.println(xbee.getResponse().getErrorCode());
-  } else {
-    // local XBee did not provide a timely TX Status Response -- should not happen
-    flashLed(errorLed, 2, 50);
-  }
+    else if(xbee.getResponse().isError()){
+    
+    }
+    else{
+        //XBee did not provide a timely TX status response
+        //This is rare and shouldn't really happen
 
-  delay(1000);
+    }
+
+    delay(1000);
+    digitalWrite(pinLED, LOW);
+    delay(1000);
+
 }
