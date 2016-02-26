5992ee56ae4978494db80c814caccb0ff4c6376d
Shantanu Kanvinde <smkanvinde@utexas.edu>
2016-02-26 16:45:05 -0600

    accel and mag functionality added to imu

--- imu/main.cpp
+++ imu/main.cpp
@@ -1,15 +1,23 @@
 #include "mbed/mbed.h"
 #include "L3GD20.h"
+#include "LSM303DLHC.h"
 #include "config.h"
 
+
+static float DELAY = 0.5;
 L3GD20 gyro(I2C_SDA, I2C_SCL);
 static DigitalOut led(LED3);
 float g1;
 float g2;
 float g3;
-bool data = false;
 Serial *log_serial;
-
+LSM303DLHC magAccel(I2C_SDA, I2C_SCL);
+float a1;
+float a2;
+float a3;
+float m1;
+float m2;
+float m3;
 /*static void blinky(void) {
     static DigitalOut led(LED1);
     static DigitalOut led2(LED2);
@@ -32,13 +40,11 @@ int main(void) {
 //    char cmd[2];
     while(true){
         led = !led;
-   /*
-     * figure out pyocd
-     * get this to flash
-     * figure out why read is not getting called
-     */
-        wait(0.5);
-        data = gyro.read(&g1, &g2, &g3);
-        if (data) log_serial->printf("x: %.2f  y: %.2f  z: %.2f\n\r", g1, g2, g3);
+        wait(DELAY);
+        gyro.read(&g1, &g2, &g3);
+        magAccel.read(&a1, &a2, &a3, &m1, &m2, &m3);
+        log_serial->printf("gyro x: %.2f  y: %.2f  z: %.2f     ", g1, g2, g3);
+        log_serial->printf("accel dx: %.2f dy: %.2f  dz: %.2f    ", a1, a2, a3);
+        log_serial->printf("mag dx: %.2f  dy: %.2f  dz: %.2f\n\r", m1, m2, m3);
     }
 }
