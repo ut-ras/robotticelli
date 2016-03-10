2ed959cbaae3597cf5e008df7dd9afa6adff877f
Miccah Castorina <m.castorina93@gmail.com>
2016-03-09 21:34:53 -0600

    WIP of round solver using rtree.

--- venus/rounds.py
+++ venus/rounds.py
@@ -13,10 +13,58 @@
 # Outputs:
 #       - set(([(x, y, can number)], [(color, can number)]))
 
+from rtree import index
+import random
 import argparse
 
+def solve_rounds(pixels, number_of_cans, max_pixels_per_can):
+    idx = index.Index()
+
+    # Insert all pixels into the rtree
+    for y in range(pixels.shape[0]):
+        for x in range(pixels.shape[1]):
+            idx.insert(pixels[y][x], (x, y, x, y), obj=[(x, y, pixels[y][x])])
+    w, h = pixels.shape()
+
+    # left bottom right top
+    bbox = (0, 0, h, w)
+
+    performed_merge = True
+    level = 0
+    num_clusters = h*w  # TODO: don't count empty pixels
+    max_per_cluster = 100
+    while performed_merge:
+        performed_merge = False
+
+        objs = idx.intersection(bbox, objects=True)
+        # random dist
+        rdist = random.sample(objs, max_per_cluster)
+
+        # cluster into next level
+        next_tree = idx.Index()
+        for center in rdist:
+            new_clusters = [[] for _ in range(number_of_cans)]
+            for cluster in idx.nearest(center.bbox, max_per_cluster, objects=True):
+                # (x, y, color)
+                obj = cluster.object
+                if len(obj) < max_per_cluster:
+                    new_clusters[cluster.id].append(obj)
+                    performed_merge = True
+                idx.delete(cluster.id, cluster.bbox)
+
+            for k, v in enumerate(new_clusters):
+                x, y, _ = map(lambda x: sum(x)/len(x), zip(*new_clusters))
+                next_tree.insert(k, (x, y, x, y), obj=v)
+
+        level += 1
+
 def main():
-    pass
+    parser = argparse.ArgumentParser('venus')
+    parser.add_argument('-i', '--image', type=str)
+    parser.add_argument('-m', '--max-pixels-per-can', type=int, default=100)
+    parser.add_argument('-n', '--number-of-cans', type=int, default=4)
+
+    args = parser.parse_args()
 
 if __name__ == '__main__':
     main()
