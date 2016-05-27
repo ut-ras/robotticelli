09c054274fa495fd95a094e25458f76a333bc799
AaronPTB <aaron.ptb@gmail.com>
2016-05-26 23:33:30 -0500

    angle calculator added (broken)

---
+++ venus/calc_angles.py
@@ -0,0 +1,71 @@
+import numpy as np
+from numpy import hypot as hyp
+import matplotlib.pyplot as plt
+
+
+H  = 10   #Wall Height
+W  = 20   #Wall Width
+g  = 9.81 #Acceleration of gravity in m/s^2
+m  = 1   #Assuming the robot weights 10 kg
+
+h  = .5   #Robot height
+w  = .5   #Robot width 
+
+b  = W * .0001    #Buffer in meters between the edge of the wall and the edge of the mural   
+
+def calc_angle(x, y): 
+	xp = W - x
+	yp = H - y
+
+	tan1 =  yp/xp
+	tan2 = -yp/x
+	tan3 =  y/x
+	tan4 = -y/xp
+
+	sin1 = yp/hyp(xp, yp)
+	sin2 = yp/hyp(x , yp)
+	sin3 = -y/hyp(x , y )
+	sin4 = -y/hyp(xp, y )
+
+	cos1 = xp/hyp(xp, yp)
+	cos2 = -x/hyp(x , yp)
+	cos3 = -x/hyp(x , y )
+	cos4 = xp/hyp(xp, y )
+
+	## Calculate dummy variables for tension
+	T3 = 0 * x/W 
+	T4 = 0 - T3
+
+	## Calculate other two tensions
+	T1 = np.abs((m*g + T3*(tan2*cos3 - sin2) + T4*(tan2*cos4 - sin4))/(sin1 - tan2*cos1))
+	T2 = np.abs((m*g + T3*(tan1*cos3 - sin3) + T4*(tan1*cos4 - sin4))/(sin2 - tan1*cos2))
+
+	## Calculate the rotation of the robot
+	phi_top = (-h*T1*cos1 + w*T4*sin2 - h*T2*cos2 + w*T4*sin4 - .5*w*m*g)
+	phi_bot = ( h*T1*sin1 + h*T2*sin2 + w*T2*cos2 - w*T4*cos4 + .5*h*m*g)
+
+	angle = np.arctan2(phi_top, phi_bot) 
+
+	return [np.cos(angle), np.sin(angle)]
+
+x_axis = np.linspace(b, W - b, 40)
+y_axis = np.linspace(b, H - b, 40)
+
+
+## For vector plot
+
+X, Y, EX, EY = [], [], [], []
+for x in x_axis:
+	for y in y_axis:
+		X.append(x)
+		Y.append(y)
+		EX.append(calc_angle(x, y)[0])
+		EY.append(calc_angle(x, y)[1])
+plt.quiver(X,Y,EX,EY)
+
+## For heatmap
+
+# out = calc_angle(x_axis[:,None], y_axis[None,:])
+# plt.pcolor(out,cmap=plt.cm.Reds)
+
+plt.show()
