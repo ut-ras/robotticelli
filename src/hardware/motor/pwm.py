d136e854de61725ab479e2aa0658cbc0112bb406
Aaron Evans <aaron.ptb@gmail.com>
2017-01-26 16:53:19 -0600

    various updates

--- src/hardware/motor/pwm.py
+++ src/hardware/motor/pwm.py
@@ -1,5 +1,6 @@
 from hardware.motor.motor import Motor_PWM
 from hardware.motor.conf import MAX_ENCODER_STEPS
+from hardware.motor.encoder import Encoder
 
 encoder_total_steps = 0
 
@@ -8,10 +9,15 @@ encoder_total_steps = 0
 motor = Motor_PWM(0,1)
 
 def run(encoder_steps):
-    direction = 0;
-    if encoder_total_steps < 0:
-        direction = -1
-    elif encoder_total_steps > 0:
-        direction = 1;
+    direction = 0
+    encoder_total_steps = 0
 
-    ##TODO: implement turning, track encoder_total_steps
+    motor.changeSpeed(90 * (1 +  encoder_steps/MAX_ENCODER_STEPS))
+    while encoder_total_steps < encoder_steps:
+        encoder_total_steps = encoder.readSteps()
+
+    #TODO: change this algorithm to work with kalman filter and PID  
+    #Motionless
+    motor.changeSpeed(90)
+    #Reset for next run 
+    encoder.resetSteps() 
