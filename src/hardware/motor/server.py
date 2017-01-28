a1ce8d1ee0b6606156c4714b8725b4b7fe875027
Aaron Evans <aaron.ptb@gmail.com>
2017-01-28 15:45:01 -0600

    update

--- src/hardware/motor/server.py
+++ src/hardware/motor/server.py
@@ -12,12 +12,11 @@ def run_step():
     ## The encoder will measure how much line has been pulled in,
     ## once it has stepped [encoder_steps] time, the program will
     ## message the robot RPi that this motor is ready for instructions
-    
-    form = dict(request.form)
-    print(form['turn_ratio'][0])
-    encoder_steps = MAX_ENCODER_STEPS * float(form['turn_ratio'][0])
 
-    #pwm.run(encoder_steps)
+    form = dict(request.form)
+    #Extracts the turn ratio from the form being sent
+    encoder_steps = MAX_ENCODER_STEPS * float(form['turn_ratio'][0])
+    pwm.run(encoder_steps)
 
     return jsonify({"response": "Hello!"})
 
