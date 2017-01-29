203898ca1e431f55b78dfaa2c93e926b696407eb
Aaron Evans <aaron.ptb@gmail.com>
2017-01-28 21:45:54 -0600

    update

--- src/hardware/motor/pwm.py
+++ src/hardware/motor/pwm.py
@@ -14,18 +14,18 @@ encoder_total_steps = 0
 
 def run(needed_encoder_steps):
     print('bloop')
-    # global motor
-    #
-    # direction = 0
-    # encoder_total_steps = 0
-    # motor.changeSpeed(90 * (1 +  needed_encoder_steps/MAX_ENCODER_STEPS))
-    #
-    # while encoder_total_steps < needed_encoder_steps:
-    #     encoder_total_steps = encoder.readSteps()
-    #
-    # #TODO: change this algorithm to work with kalman filter and PID
-    # #Motionless
-    # motor.changeSpeed(90)
-    # #Reset for next run
-    # encoder.resetSteps()
+    global motor
+
+    direction = 0
+    encoder_total_steps = 0
+    motor.changeSpeed(90 * (1 +  needed_encoder_steps/MAX_ENCODER_STEPS))
+
+    while encoder_total_steps < needed_encoder_steps:
+        encoder_total_steps = encoder.readSteps()
+
+    #TODO: change this algorithm to work with kalman filter and PID
+    #Motionless
+    motor.changeSpeed(90)
+    #Reset for next run
+    encoder.resetSteps()
     thread.start_new_thread(send_ready, ())
